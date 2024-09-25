9fefaa57ed0510b70825bcc9aacb441d1fe44211
Huy Huy <nhathuych@gmail.com>
2024-09-25 23:42:12 +0700

    add model

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,10 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "articles#index"
+
+  post :search, to: "search#index", as: :search
+  post "search/suggestions", to: "search#suggestions", as: :search_suggestions
+
+  resources :articles
 end
